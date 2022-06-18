8f9ff8fccec313488ae334cd34ac7bea6b4c5db9
daniil <dddmitriev@hex.team>
2022-06-18 15:19:51 +0300

    Update UDP server

--- src/Net/Server.cpp
+++ src/Net/Server.cpp
@@ -1,5 +1,6 @@
 #include "Net/Server.h"
 
+#include <atomic>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -18,6 +19,7 @@ public:
                             &&func) override;
 
     bool start() override;
+    bool stop() override;
 
     // constructors
 public:
@@ -25,14 +27,20 @@ public:
     UDPServerImpl(Address address, Port port);
     virtual ~UDPServerImpl();
 
+    // in-class interface
+private:
+    bool mainLoop();
+
     // data
 private:
     std::function<std::string(const std::string &)> mHandler;
-    utils::ThreadPool tastList;
+    utils::ThreadPool taskList;
     int mSocket;
+    std::atomic_bool mActive;
 };
 
-UDPServerImpl::UDPServerImpl(Address address, Port port) : tastList(4) {
+UDPServerImpl::UDPServerImpl(Address address, Port port)
+        : taskList(4), mActive(false) {
     // LoggerDebug __logger_debug;
 
     LOGI() << "Create Server on " << (int)port;
@@ -61,6 +69,15 @@ UDPServerImpl::UDPServerImpl(Address address, Port port) : tastList(4) {
         throw;
     }
     LOGI() << "Binded No: " << mSocket;
+    // Set socket non-block
+    timeval read_timeout;
+    read_timeout.tv_sec = 1;
+    read_timeout.tv_usec = 0;
+    setsockopt(mSocket,
+               SOL_SOCKET,
+               SO_RCVTIMEO,
+               &read_timeout,
+               sizeof read_timeout);
 }
 UDPServerImpl::~UDPServerImpl() { close(mSocket); }
 
@@ -69,15 +86,15 @@ bool UDPServerImpl::setHandler(
     mHandler = std::move(func);
     return true;
 }
-
-bool UDPServerImpl::start() {
+bool UDPServerImpl::mainLoop() {
     LoggerDebug __logger_debug;
 
     struct sockaddr_in client_addr;
     int client_len = sizeof(client_addr);
     const size_t size = 512;
     char buffer[size];
-    for (;;) {
+    mActive = true;
+    while (mActive) {
         // Wait for the message
         int bytesRecieved = recvfrom(mSocket,
                                      buffer,
@@ -86,12 +103,12 @@ bool UDPServerImpl::start() {
                                      (struct sockaddr *)&client_addr,
                                      (socklen_t *)&client_len);
         if (bytesRecieved == -1) {
-            LOGW() << "Could not get data";
+            LOGW() << "No data";
             continue;
             // return false;
         }
 
-        tastList.addTask([str = std::string(buffer),
+        taskList.addTask([str = std::string(buffer),
                           out_addr = client_addr,
                           this]() {
             std::string ans = mHandler(str);
@@ -103,6 +120,20 @@ bool UDPServerImpl::start() {
                    sizeof out_addr);
         });
     }
+
+    // close socket at the end
+    return true;
+}
+
+bool UDPServerImpl::start() {
+    taskList.addTask([this]() {
+        this->mainLoop();
+    });
+    return true;
+}
+
+bool UDPServerImpl::stop() {
+    mActive = false;
     return true;
 }
 
@@ -114,7 +145,7 @@ Server::create(ServerType type, Address address, Port port) {
     switch (type) {
     case ServerType::TCP:
         LOGI() << "Create TCP server";
-        throw;
+        return nullptr;
 
     case ServerType::UDP:
         LOGI() << "Create UDP server";
